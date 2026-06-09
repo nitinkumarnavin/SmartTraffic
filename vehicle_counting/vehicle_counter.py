0bbef15cb3ce4a628260d732f6d75c21b88d5c2b
jatink16 <jkola5416@gmail.com>
2026-06-09 08:46:16 +0530

    .

---
+++ vehicle_counting/vehicle_counter.py
@@ -0,0 +1,92 @@
+from ultralytics import YOLO
+import cv2
+
+# Load model
+model = YOLO("yolov8n.pt")
+
+video_path = r"dataset/traffic_videos/M6 Motorway Traffic [PNCJQkvALVc].mp4"
+
+cap = cv2.VideoCapture(video_path)
+
+vehicle_classes = {
+    2: "car",
+    3: "motorcycle",
+    5: "bus",
+    7: "truck"
+}
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    results = model(frame)
+
+    car_count = 0
+    bike_count = 0
+    bus_count = 0
+    truck_count = 0
+
+    for box in results[0].boxes:
+
+        cls = int(box.cls[0])
+
+        if cls == 2:
+            car_count += 1
+        elif cls == 3:
+            bike_count += 1
+        elif cls == 5:
+            bus_count += 1
+        elif cls == 7:
+            truck_count += 1
+
+    annotated_frame = results[0].plot()
+
+    cv2.putText(
+        annotated_frame,
+        f"Cars: {car_count}",
+        (20, 40),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0, 255, 0),
+        2
+    )
+
+    cv2.putText(
+        annotated_frame,
+        f"Bikes: {bike_count}",
+        (20, 80),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0, 255, 0),
+        2
+    )
+
+    cv2.putText(
+        annotated_frame,
+        f"Buses: {bus_count}",
+        (20, 120),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0, 255, 0),
+        2
+    )
+
+    cv2.putText(
+        annotated_frame,
+        f"Trucks: {truck_count}",
+        (20, 160),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0, 255, 0),
+        2
+    )
+
+    cv2.imshow("Vehicle Counting", annotated_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
